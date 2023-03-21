1c84ef054e87c2177ce808d33f73f672f9abeb7e
nvaradha <nvaradha@iu.edu>
2023-03-21 16:51:22 -0400

    initial commit got customer post working

---
+++ src/main/java/edu/iu/p565/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,26 @@
+package edu.iu.p565.customerservice.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import edu.iu.p565.customerservice.model.Customer;
+
+@Repository
+public class CustomerRepository {
+
+    private List<Customer> customers = new ArrayList<>();
+
+    public List<Customer> findAll() {
+        return customers;
+    }
+    
+    public int create(Customer customer) {
+        int id = customers.size() + 1; //generate id
+        customer.setId(id);
+
+        customers.add(customer); //add customer to list
+        return id;
+    }
+}
